81bd0c51d8964edf61fdf5c29a3cb84e9afd7fd5
ivcproject <82091198+ivcproject@users.noreply.github.com>
2023-04-03 10:39:48 +0300

    Update glpi-report1.sql

--- glpi-report1.sql
+++ glpi-report1.sql
@@ -1,3 +1,14 @@
+/*    ==Параметры сценариев==
+
+    Версия исходного сервера : SQL Server 2016 (13.0.4001)
+    Выпуск исходного ядра СУБД : Выпуск Microsoft SQL Server Standard Edition
+    Тип исходного ядра СУБД : Изолированный SQL Server
+
+    Версия целевого сервера : SQL Server 2017
+    Выпуск целевого ядра СУБД : Выпуск Microsoft SQL Server Standard Edition
+    Тип целевого ядра СУБД : Изолированный SQL Server
+*/
+
 USE [GLPI-Linked]
 GO
 /****** Object:  StoredProcedure [dbo].[GLPI_problems2]    Script Date: 13.02.2023 14:00:47 ******/
@@ -92,7 +103,10 @@ REPLACE(REPLACE((SELECT GROUP_CONCAT(content SEPARATOR " ") AS content  FROM glp
 (SELECT  GROUP_CONCAT(CONCAT(IFNULL(CONCAT(glpi_usertitles.name,"<br>"),""), realname," ", firstname," <br>", phone,"")) AS "Исполнитель" FROM glpi_tickets_users
  INNER JOIN glpi_users ON glpi_tickets_users.users_id=glpi_users.id 
  LEFT JOIN glpi_usertitles ON glpi_usertitles.id=glpi_users.usertitles_id
- WHERE type=1 AND tickets_id=glpi_tickets.id ) AS initi, "" as software
+ WHERE type=1 AND tickets_id=glpi_tickets.id ) AS initi,
+ (SELECT ifnull(GROUP_CONCAT(glpi_softwares.name SEPARATOR ", "), "") from glpi_items_tickets 
+								LEFT JOIN glpi_softwares ON glpi_softwares.id=glpi_items_tickets.items_id
+								WHERE itemtype= "Software" AND tickets_id=glpi_tickets.id) AS software 
 FROM glpi_tickets 
 LEFT JOIN glpi_entities ON glpi_tickets.entities_id = glpi_entities.id
 LEFT JOIN glpi_plugin_fields_ticketfailures ON glpi_plugin_fields_ticketfailures.items_id=glpi_tickets.id
@@ -100,7 +114,7 @@ LEFT JOIN glpi_plugin_fields_failcategoryfielddropdowns ON glpi_plugin_fields_fa
 LEFT JOIN (SELECT tickets_id, COUNT(problems_id) as countpr FROM glpi_problems_tickets GROUP BY tickets_id) pt on pt.tickets_id=glpi_tickets.id
 
 WHERE glpi_tickets.is_deleted<>TRUE  AND glpi_plugin_fields_failcategoryfielddropdowns.id>4 
-AND (glpi_tickets.name not like "%тест%" and glpi_tickets.name not like "%test%") ' +
+AND (glpi_tickets.name not like "%тест%" and glpi_tickets.name not like "%2222%") ' +
 'AND (UNIX_TIMESTAMP(glpi_tickets.DATE)<=' + @stime2 + ' ' +
 ' AND ( UNIX_TIMESTAMP(glpi_tickets.solvedate)>=' + @stime1 + ' OR glpi_tickets.solvedate IS NULL ))' +
  ' ORDER BY glpi_tickets.DATE desc
@@ -163,7 +177,7 @@ REPLACE(REPLACE((SELECT GROUP_CONCAT(content SEPARATOR " ") AS content  FROM glp
 								WHERE itemtype= "Software" AND changes_id=glpi_changes.id) AS software
  FROM glpi_changes
 LEFT JOIN glpi_entities ON glpi_changes.entities_id = glpi_entities.id
-WHERE glpi_changes.is_deleted=0 AND (glpi_changes.name not like "%тест%" and glpi_changes.name not like "%test%") ' +
+WHERE glpi_changes.is_deleted=0 AND (glpi_changes.name not like "%тест%" and glpi_changes.name not like "%222%") ' +
 'AND (UNIX_TIMESTAMP(glpi_changes.DATE)<=' + @stime2 + ' ' +
 ' AND ( UNIX_TIMESTAMP(glpi_changes.solvedate)>=' + @stime1 + ' OR glpi_changes.solvedate IS NULL ))' +
  ' ORDER BY glpi_changes.DATE desc'
